f57f480ea54c412ff21b72402ff5a893a19bc108
“XDD” <yanxiaodong0524>
2025-08-27 21:45:54 +0800

    learn_hardhat：配置网络

--- learn_hardhat/hardhat.config.js
+++ learn_hardhat/hardhat.config.js
@@ -4,9 +4,14 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  //配置网络
   networks:{
     sepolia:{
       url:`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts:[process.env.PK],
+    },
+    mainnet:{
+      url:`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts:[process.env.PK],
     }
   }
